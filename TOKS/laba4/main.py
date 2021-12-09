f8506ce0eb29bccbd3c8b322ecb4e8a145ea84fb
Alexander Krylov <71841167+confusedqqw@users.noreply.github.com>
2021-12-10 02:45:26 +0300

    Update main.py

--- TOKS/laba4/main.py
+++ TOKS/laba4/main.py
@@ -9,7 +9,7 @@ import threading
 import random
 
 threads = 0
-string_buf = []
+stringBuf = []
 string_send_buf = ""
 send_flag = 0
 start_flag = 0
@@ -17,176 +17,47 @@ fl1 = 0
 ser = None
 flag = 0
 state = 2
-state_output = 2
-state_prev = 0
-attempts_max = 10
-jam = 0
+outState = 2
+prevState = 0
+maxAttempts = 10
+jamSignal = 0
 clock = 0
-no_collision = 0
+collision = 0
 not_connected = 0
+pick_flag = 0
 
 
 def Connect():
-    global start_flag
+    global pick_flag
     global ser
     global thread
     try:
-        ser = serial.Serial("COM2", timeout=0)
+        ser = serial.Serial("COM3",timeout=0)
+        pick_flag = 1
     except Exception:
-        if ser is None:
-            try:
-                ser = serial.Serial("COM3", timeout=0)
-            except Exception:
-                if ser is None:
-                   Error("Error opening ports")
-            else:
-                start_flag = 1
-    else:
-        start_flag = 1
-    finally:
-        if start_flag == 1:
-            thread.start()
+        if pick_flag == 0:
+            Error("Port is closed")
+    if pick_flag == 1:
+        try:
+            ser = serial.Serial("COM4",timeout=0)
+            pick_flag = 1
+        except Exception:
+            if pick_flag == 0:
+                Error("Port is closed")
+    thread.start()
 
 def Error(text):
     msg = text
     showinfo(title='Error', message=msg)
 
-def Input():
-    global ser
-    global no_collision
-    global clock
-    global state_output
-    global state_prev
-    global jam
-    time1 = 0
-    buf = []
-    while True:
-        len_ = 0
-        while len_ < 1 and no_collision != 1:
-            str_ = ser.readline().decode()
-            time.sleep(0.2)
-            if str_ == '1':
-                state_prev = state_output
-                state_output = 1
-            if str_ == '0':
-                state_prev = state_output
-                state_output = 0
-            if clock == 1 and len(str_) == 0:
-                time2 = time.time() - time1
-                if time2 > 0.3:
-                    no_collision = 1
-                    clock = 0
-                    time1 = 0
-            len_ = len(str_)
-        if (str_ != '0' and str_ != '1') or no_collision == 1:
-            print(str_)
-            if len(buf) == 0 and no_collision == 0 and str_ != 'c':
-                buf.append(str_)
-                time1 = time.time()
-                clock = 1
-            elif str_ == 'c':
-                print("jam signal")
-                buf.clear()
-                time1 = 0
-                clock = 0
-            elif no_collision == 1:
-                str_output = buf.pop()
-                bit_list = [str(i) for i in list(str_output)]
-                decodeMessage(bit_list)
-                buf.clear()
-                no_collision = 0
-                time1 = 0
-                clock = 0
-            else:
-                str_output = buf.pop()
-                state_output = 0
-                bit_list = [str(i) for i in list(str_output)]
-                decodeMessage(bit_list)
-                buf.append(str_)
-                time1 = time.time()
-                clock = 1
-
-
-def decodeMessage(message):
-    global state_prev
-    global state_output
-    bit_str = list(message)
-    bit_str_tmp = ''.join(str(i) for i in bit_str)
-    if state_output == 1 and state_prev == 0:
-        bit_str_tmp = bit_str_tmp[:len(bit_str_tmp) - 1]
-        state_output = 2
-    elif state_output == 2 and state_prev == 0:
-        bit_str_tmp = ''
-        bit_str_tmp = bit_str_tmp.join(str(i) for i in bit_str)
-        state_output = 2
-    elif state_prev == 0 and state_output == 0:
-        state_output = 2
-    string_buf.append(bit_str_tmp)
-    return bit_str_tmp
-
-thread = threading.Thread(target=Input)
-thread.daemon = True
-
 def send():
     global ser
-    str_ = inputEntry.get()
-    if len(str_) > 0:
-        sending(str_)
+    mes = inputEntry.get()
+    if len(mes) > 0:
+        writeMessage(mes)
     inputEntry.config(validate="none")
     inputEntry.delete(0, END)
     inputEntry.config(validate="key")
-
-def sending(str_):
-    global state
-    global ser
-    bit_str = [int(i) for i in list(str_)]
-    state = 2
-    attempts = 0
-    debugListbox.insert(END,"User input: "+ str_)
-    print("user input: " + str_)
-    while state == 2:
-        state == 0
-        bit_list = makeFrame(bit_str)
-        while True:
-            while listen_to_channel() != 0:
-                print("channel is busy")
-            print("channel is free")
-            if state == 2:
-                ser.write('0'.encode())
-                time.sleep(0.1)
-            if state == 1:
-                ser.write('1'.encode())
-                time.sleep(0.1)
-            tmp = ''
-            tmp = tmp.join(str(c) for c in list(bit_list))
-            ser.write(tmp.encode())
-            time.sleep(0.1)
-            if collision_random() == 1:
-                ser.write('c'.encode())
-                time.sleep(0.1)
-                attempts += 1
-                if attempts < attempts_max:
-                    time.sleep(random.randint(0, 2 ** min(attempts, 10)) * 0.01)
-                else:
-                    break
-            else:
-                break
-        debugListbox.insert(END, tmp + ':' + '*' * attempts)
-        attempts = 0
-
-
-def PortDisplay():
-    global send_flag
-    global not_connected
-    while len(string_buf) > 0:
-        str_ = string_buf.pop(0)
-        if str_ != '':
-            outputListbox.insert(0, str_)
-    if send_flag:
-        send()
-        send_flag = 0
-    root.after(100, PortDisplay)
-
 
 def fillFrame(message):
     while len(message) != 23:
@@ -199,23 +70,23 @@ def makeFrame(message):
         frame = message
         state = 0
         return frame
-    if len(message) == 22:
-        message.append(0)
+    if len(message) == 23:
+        #message.append(0)
         frame = message
         state = 1
         return frame
-    if len(message) > 22 or state == 2:
-        frame = message[len(message) - 23:]
-        del message[len(message) - 23:]
+    if len(message) > 23 or state == 2:
+        frame = message[:23]
+        del message[:23]
         state = 2
         return frame
 
 
-def listen_to_channel():
+def channelBusy():
     return 0 if random.randint(1, 5) == 1 else 1
 
 
-def collision_random():
+def makeCol():
     return random.randint(0, 1)
 
 
@@ -223,6 +94,133 @@ def out_flag():
     global send_flag
     send_flag = 1
 
+def acceptMessage():
+    global ser
+    global collision
+    global clock
+    global jamSignal
+    global outState
+    global prevState
+    time1 = 0
+    buf = []
+    while True:
+        len_ = 0
+        while len_ < 1 and collision != 1:
+            mes = ser.readline().decode()
+            time.sleep(0.2)
+            if mes == '1':
+                prevState = outState
+                outState = 1
+            if mes == '0':
+                prevState = outState
+                outState = 0
+            if clock == 1 and len(mes) == 0:
+                time2 = time.time() - time1
+                if time2 > 0.3:
+                    collision = 1
+                    clock = 0
+                    time1 = 0
+            len_ = len(mes)
+        if (mes != '0' and mes != '1') or collision == 1:
+            if mes!="":
+                print("received: " + mes)
+            if len(buf) == 0 and collision == 0 and mes != 'c':
+                buf.append(mes)
+                time1 = time.time()
+                clock = 1
+            elif mes == 'c':
+                print("jam signal")
+                buf.clear()
+                time1 = 0
+                clock = 0
+            elif collision == 1:
+                str_output = buf.pop()
+                bit_list = [str(i) for i in list(str_output)]
+                decodeMessage(bit_list)
+                buf.clear()
+                collision = 0
+                time1 = 0
+                clock = 0
+            else:
+                str_output = buf.pop()
+                outState = 0
+                bit_list = [str(i) for i in list(str_output)]
+                decodeMessage(bit_list)
+                buf.append(mes)
+                time1 = time.time()
+                clock = 1
+
+
+def decodeMessage(mes):
+    global prevState
+    global outState
+    bit_str = list(mes)
+    tmp = ''.join(str(i) for i in bit_str)
+    if outState == 1 and prevState == 0:
+        tmp = tmp[:len(tmp) - 1]
+        outState = 2
+    elif outState == 2 and prevState == 0:
+        tmp = ''
+        tmp = tmp.join(str(i) for i in bit_str)
+        outState = 2
+    elif prevState == 0 and outState == 0:
+        outState = 2
+    stringBuf.append(tmp)
+    return tmp
+
+thread = threading.Thread(target=acceptMessage)
+thread.daemon = True
+
+def writeMessage(mes):
+    global state
+    global ser
+    bit_str = [int(i) for i in list(mes)]
+    state = 2
+    attempts = 0
+    debugListbox.insert(END,"User input: " + mes)
+    print("user input: " + mes)
+    while state == 2:
+        state = 0
+        bit_list = makeFrame(bit_str)
+        while True:
+            while channelBusy() != 0:
+                print("channel is busy")
+            print("channel is free")
+            if state == 2:
+                ser.write('0'.encode())
+                time.sleep(0.1)
+            if state == 1:
+                ser.write('1'.encode())
+                time.sleep(0.1)
+            tmp = ''
+            tmp = tmp.join(str(c) for c in list(bit_list))
+            ser.write(tmp.encode())
+            time.sleep(0.1)
+            if makeCol() == 1:
+                ser.write('c'.encode())
+                time.sleep(0.1)
+                attempts += 1
+                if attempts < maxAttempts:
+                    time.sleep(random.randint(0, 2 ** min(attempts, 10)) * 0.01)
+                else:
+                    break
+            else:
+                break
+        debugListbox.insert(END, tmp + ':' + '*' * attempts)
+        attempts = 0
+
+
+def PortDisplay():
+    global send_flag
+    global not_connected
+    while len(stringBuf) > 0:
+        str_ = stringBuf.pop(0)
+        if str_ != '':
+            outputListbox.insert(0, str_)
+    if send_flag:
+        send()
+        send_flag = 0
+    root.after(100, PortDisplay)
 
 def changeBaudrate():
     global ser
@@ -279,4 +277,3 @@ sendButton.pack(side=BOTTOM)
 Connect()
 root.after(10, PortDisplay)
 root.mainloop()
-
